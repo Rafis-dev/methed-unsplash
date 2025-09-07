f6773a788869d934af7fcc89d13f2ae75100b528
Rafis Shigapov <rafis1work@gmail.com>
2025-09-07 12:04:24 +0300

    fix redirect

--- src/api/const.js
+++ src/api/const.js
@@ -3,7 +3,7 @@ export const API_URL_TOKEN = 'https://unsplash.com/oauth/token';
 export const API_URL_AUTH = 'https://unsplash.com/oauth/authorize';
 export const ACCESS_KEY = 'WBz0dIPqF3E_HeNtLYdbZw94QkM-gvQ32tXBg5yU3PE';
 export const SECRET_KEY = 'fUJ_ruEp20zrue_zQkQf1PiAcWsESxW2g5WjL1FnWNM';
-export const REDIRECT_URI = 'http://localhost:3000';
+export const REDIRECT_URI = 'https://methed-unsplash.vercel.app/';
 export const RESPONSE_TYPE = 'code';
 export const SCOPE = 'public read_user read_photos write_likes';
 
